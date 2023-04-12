787dc6895ccd2485d8328279d95bc09438975e2f
Gunvant Sharma <43336633+gunvant8008@users.noreply.github.com>
2023-04-12 17:15:41 +0100

    removed unnecessary imports

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,11 +1,13 @@
-import Counter from "@/components/counter/Counter";
-
 // const inter = Inter({ subsets: ["latin"] })
+
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <Counter description="Our Counter" defaultCount={0} />
+    <div className=" mt-10 text-center">
+      <Link className="p-4 text-2xl bg-teal-900" href="/list">
+        Go to photos list 🔗
+      </Link>
     </div>
   );
 }
