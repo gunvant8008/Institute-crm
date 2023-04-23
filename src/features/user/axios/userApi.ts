e9489d5b54302d896245ee606d2ee9e270a0cccc
Gunvant Sharma <43336633+gunvant8008@users.noreply.github.com>
2023-04-23 12:29:01 +0100

    changed the endpoint from localhost to vercel url to check msw will work or not

--- src/features/user/axios/userApi.ts
+++ src/features/user/axios/userApi.ts
@@ -3,7 +3,8 @@ import axios from "axios";
 
 const userApi = axios.create({
   // baseURL: "https://jsonplaceholder.typicode.com",
-  baseURL: "http://localhost:3000/api/",
+  // baseURL: "http://localhost:3000/api/",
+  baseURL: "https://institute-crm.vercel.app//api/",
 });
 
 export const getUsers = async (): Promise<TUser[]> => {
