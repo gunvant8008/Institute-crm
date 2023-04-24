9535d3753e32220656090a887a247e036a21d867
Gunvant Sharma <43336633+gunvant8008@users.noreply.github.com>
2023-04-24 15:40:09 +0100

    changed axios base url

--- src/features/user/axios/userApi.ts
+++ src/features/user/axios/userApi.ts
@@ -3,9 +3,9 @@ import axios from "axios";
 
 const userApi = axios.create({
   // baseURL: "https://jsonplaceholder.typicode.com",
-  baseURL: "http://localhost:3000/api/",
+  // baseURL: "http://localhost:3000/api/",
   // baseURL: "http://127.0.0.1:3000/api/"
-  // baseURL: "https://institute-crm.vercel.app/api/",
+  baseURL: "https://institute-crm.vercel.app/api/",
 });
 
 export const getUsers = async (): Promise<TUser[]> => {
