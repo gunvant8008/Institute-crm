cd9cda9d4119b6425f33334c9792a8d23afefd12
Gunvant Sharma <43336633+gunvant8008@users.noreply.github.com>
2023-04-24 11:58:46 +0100

    solved build time error form Orders component import

--- src/pages/new.page.tsx
+++ src/pages/new.page.tsx
@@ -1,11 +1,8 @@
-import Orders from "@/components/cards/Orders";
 import AddUser from "@/features/user/components/forms/AddUser";
-import NewPhoto from "@/components/forms/NewPhoto";
 
 const NewPage = () => {
   return (
     <>
-      {/* <Orders /> */}
       <AddUser />
     </>
   );
